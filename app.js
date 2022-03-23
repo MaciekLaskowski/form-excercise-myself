c19fe0e7136d044c99d05c7b43091e696def39e9
Maciej Laskowski <mlaskowski11@gmail.com>
2022-03-23 20:26:00 +0100

    started forming is statement

--- app.js
+++ app.js
@@ -5,3 +5,19 @@ const mailInput = document.getElementById("mail-input")
 const btn = document.querySelector(".btn")
 const list = document.querySelector(".list")
 
+myForm.addEventListener("submit", onSubmit);
+
+function onSubmit(e){
+    e.preventDefault();
+    if(nameInput.value === "" && mailInput.value === ""){
+        formText.classList.add("red");
+        formText.textContent = "Name and mail inputs can't be empty"
+    } else if(nameInput.value === ""){
+        formText.classList.add("red");
+        formText.textContent = "Name input can't be empty"
+    } else if(mailInput.value === ""){
+        formText.classList.add("red");
+        formText.textContent = "Mail input can't be empty"
+    }
+}
+
