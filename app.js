3e104656755a09e6579371561a8005769fe1c0e5
Maciej Laskowski <mlaskowski11@gmail.com>
2022-03-23 20:15:13 +0100

    creates js vars

---
+++ app.js
@@ -0,0 +1,7 @@
+const myForm = document.querySelector(".my-form")
+const formText = document.querySelector(".form-text")
+const nameInput = document.getElementById("name-input")
+const mailInput = document.getElementById("mail-input")
+const btn = document.querySelector(".btn")
+const list = document.querySelector(".list")
+
